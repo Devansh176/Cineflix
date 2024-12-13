e90a98e38d53c4ccd56d08ccbed67012c95bb65f
Devansh <devanshdhopte@gmail.com>
2024-12-13 23:07:07 +0530

    Implemented Cached Network Image package

--- lib/widgets/movies/trendingMovies.dart
+++ lib/widgets/movies/trendingMovies.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cineflix/description.dart';
 import 'package:flutter/material.dart';
 
@@ -12,6 +13,12 @@ class TrendingMovies extends StatelessWidget {
     final height = screenSize.height;
     final fontSize = width * 0.05;
     final padding = width * 0.05;
+
+    String getValidImageUrl(String? url) {
+      return (url != null && url.isNotEmpty)
+          ? 'https://image.tmdb.org/t/p/w500$url'
+          : 'https://via.placeholder.com/300';
+    }
 
     return Container(
       padding: EdgeInsets.only(top: padding * 0.8, left: padding * 0.8,),
@@ -57,11 +64,14 @@ class TrendingMovies extends StatelessWidget {
                           height: height * 0.25,
                           decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(50,),
-                            image: DecorationImage(
-                              image: NetworkImage(
-                              'https://image.tmdb.org/t/p/w500'+trending[index]['poster_path']
-                              ),
+                          ),
+                          child: CachedNetworkImage(
+                            imageUrl: getValidImageUrl(trending[index]['poster_path'],),
+                            fit: BoxFit.cover,
+                            placeholder: (context, url) => Center(
+                              child: CircularProgressIndicator(),
                             ),
+                            errorWidget: (context, url, error) => Icon(Icons.error, color: Colors.grey),
                           ),
                         ),
                         SizedBox(height: height * 0.025,),
